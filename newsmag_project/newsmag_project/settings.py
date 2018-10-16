e87eb1935773a2a72d56d4455ce981a310969976
ElToro1966 <mail@ericeikrem.com>
2018-10-16 11:22:55 +0200

    Add templates for sign-up, login, logout.

--- newsmag_project/newsmag_project/settings.py
+++ newsmag_project/newsmag_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'newsmag_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,5 @@ STATIC_URL = '/static/'
 # Added in project
 
 AUTH_USER_MODEL = 'users.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
