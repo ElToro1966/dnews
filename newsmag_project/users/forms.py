ecd05376b30d09de5d739e0c03c6f35ff3861c8e
ElToro1966 <mail@ericeikrem.com>
2018-10-17 16:35:27 +0200

    Add email to form.

--- newsmag_project/users/forms.py
+++ newsmag_project/users/forms.py
@@ -7,12 +7,12 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields
+        fields = ('username', 'email', ) 
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username', 'email', ) 
 
 
